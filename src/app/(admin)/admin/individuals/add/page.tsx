1f78cca628a5cf3564dff6dcb0c2e4ad9c105131
kbits000 <69712340+kbits000@users.noreply.github.com>
2026-02-13 21:25:23 +0300

    update add individual submission form functionality

--- src/app/(admin)/admin/individuals/add/page.tsx
+++ src/app/(admin)/admin/individuals/add/page.tsx
@@ -4,17 +4,7 @@ import AdminSidebar from "@/components/admin_page/admin_sidebar";
 import Box from "@mui/material/Box";
 import AdminBreadcrumbs from "@/components/admin_page/admin_breadcrumbs";
 import Footer from "@/components/footer/Footer";
-import Form from 'next/form'
-import TextField from '@mui/material/TextField';
-import {PrintStatement} from "@/lib/actions/admin_server_actions";
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import SaveButton from "@/components/admin_page/save_button";
+import IndividualSubmissionForm from "@/components/admin_page/individuals_page/individual_submission_form";
 
 export default async function IndividualsAddPage() {
     return (
@@ -23,39 +13,7 @@ export default async function IndividualsAddPage() {
                 <Box sx={{px:4, py: 0}}>
                     <AdminBreadcrumbs breadcrumbsList={[{text: 'المشرف', path: '/admin'}, {text: 'الافراد', path: '#'}]}/>
                     <h1>اضافة افراد جدد</h1>
-                    <Form action={PrintStatement}>
-                            <Stack
-                                useFlexGap
-                                spacing={{md:2}}
-                                direction={{ xs: 'column', sm: 'row' }}
-                                sx={{ flexWrap: 'wrap' }}
-                            >
-                                <TextField required name="first_name_field" id="first_name" placeholder={"الأسم الاول"} variant="outlined" margin="normal" />
-                                <TextField name="parent_name_field" id="parent_name" placeholder={"اسم الاب"} variant="outlined" margin="normal" />
-                                <TextField name="grandparent_name_field" id="grandparent_name" placeholder={"اسم الجد"} variant="outlined" margin="normal" />
-                                <TextField name="last_name_field" id="last_name" placeholder={"الأسم الاخير"} variant="outlined" margin="normal" />
-                                <FormControl sx={{ mt: 2 }}>
-                                    <FormLabel id="gender-label">الجنس</FormLabel>
-                                    <RadioGroup
-                                        aria-labelledby="gender-label"
-                                        defaultValue="غير معلوم"
-                                        name="gender_field"
-                                        row
-                                    >
-                                        <FormControlLabel value="ذكر" control={<Radio />} label="ذكر" />
-                                        <FormControlLabel value="أنثى" control={<Radio />} label="أنثى" />
-                                        <FormControlLabel value="غير معلوم" control={<Radio />} label="غير معلوم" />
-                                    </RadioGroup>
-                                </FormControl>
-                            </Stack>
-                        <Button
-                            type={'submit'}
-                            variant="contained"
-                        >
-                            حفظ
-                        </Button>
-                        {/*<SaveButton />*/}
-                    </Form>
+                    <IndividualSubmissionForm />
                 </Box>
                 <Footer />
             </div>
---
+++ src/components/admin_page/individuals_page/individual_submission_form.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { useState } from 'react';
+import Form from 'next/form'
+import TextField from '@mui/material/TextField';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import {PrintStatement} from "@/lib/actions/admin_server_actions";
+
+// TODO implement input validation
+export default function IndividualSubmissionForm() {
+    const [loading, setLoading] = useState(false);
+
+    return (
+        <Form action={PrintStatement}>
+            <Stack
+                useFlexGap
+                spacing={{md:2}}
+                direction={{ xs: 'column', sm: 'row' }}
+                sx={{ flexWrap: 'wrap' }}
+            >
+                <TextField required name="first_name_field" id="first_name" placeholder={"الأسم الاول"} variant="outlined" margin="normal" />
+                <TextField name="parent_name_field" id="parent_name" placeholder={"اسم الاب"} variant="outlined" margin="normal" />
+                <TextField name="grandparent_name_field" id="grandparent_name" placeholder={"اسم الجد"} variant="outlined" margin="normal" />
+                <TextField name="last_name_field" id="last_name" placeholder={"الأسم الاخير"} variant="outlined" margin="normal" />
+                <FormControl sx={{ mt: 2 }}>
+                    <FormLabel id="gender-label">الجنس</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="gender-label"
+                        defaultValue="غير معلوم"
+                        name="gender_field"
+                        row
+                    >
+                        <FormControlLabel value="ذكر" control={<Radio />} label="ذكر" />
+                        <FormControlLabel value="أنثى" control={<Radio />} label="أنثى" />
+                        <FormControlLabel value="غير معلوم" control={<Radio />} label="غير معلوم" />
+                    </RadioGroup>
+                </FormControl>
+            </Stack>
+            <Button
+                type={'submit'}
+                variant="contained"
+                onClick={() => {
+                    alert('clicked');
+                }}
+                loading={loading}
+            >
+                حفظ
+            </Button>
+        </Form>
+    )
+}

2de48bbb4ee2d0345097c377fcc0828b7df61dc7
kbits000 <69712340+kbits000@users.noreply.github.com>
2026-02-13 21:31:24 +0300

    add server action

--- src/lib/actions/admin_server_actions.ts
+++ src/lib/actions/admin_server_actions.ts
@@ -1,5 +1,15 @@
 'use server'
 
-export async function PrintStatement() {
+export async function PrintStatement(formData: FormData) {
     console.log('PrintStatement');
+    console.log(formData);
+    const rawFormData = {
+        firstNameField: formData.get('first_name_field'),
+        parentNameField: formData.get('parent_name_field'),
+        grandparentNameField: formData.get('grandparent_name_field'),
+        lastNameField: formData.get('last_name_field'),
+        genderField: formData.get('gender_field')
+    };
+
+
 }

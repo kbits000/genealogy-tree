b2dca4a023a92744f196b1e0c94fd344566c1103
kbits000 <69712340+kbits000@users.noreply.github.com>
2026-02-13 20:47:41 +0300

    add link to individuals page

--- src/app/(admin)/admin/individuals/page.tsx
+++ src/app/(admin)/admin/individuals/page.tsx
@@ -5,6 +5,8 @@ import AdminSidebar from "@/components/admin_page/admin_sidebar";
 import Box from "@mui/material/Box";
 import AdminBreadcrumbs from "@/components/admin_page/admin_breadcrumbs";
 import Footer from "@/components/footer/Footer";
+import Link from "@/components/Link";
+import Button from '@mui/material/Button';
 
 export default async function AdminIndividualsPage() {
 
@@ -13,6 +15,9 @@ export default async function AdminIndividualsPage() {
             <AdminSidebar selectedButton={'Individuals'} />
             <Box sx={{px:4, py: 0}}>
                 <AdminBreadcrumbs breadcrumbsList={[{text: 'Admin', path: '/admin'}, {text: 'Individuals', path: '#'}]}/>
+                <Button variant="contained" component={Link} href="/admin/individuals/add">
+                    اضافة فرد
+                </Button>
                 <IndividualsList/>
             </Box>
             <Footer />
